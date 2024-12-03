d4de94ba30f44bb9c4b48b8f23f9edc70a165fae
sososo <c0b231154e@edu.teu.ac.jp>
2024-12-03 16:22:03 +0900

    完成

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -161,21 +161,15 @@ def main():
                 beam = Beam(bird)            
         screen.blit(bg_img, [0, 0])
         
-        if bomb is not None:
+        for bomb in bombs:
             if bird.rct.colliderect(bomb.rct):
+            #if bomb is not None:
+                print("hit")
                 # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
                 bird.change_img(8, screen)
                 fonto = pg.font.Font(None, 80)
                 txt = fonto.render("Game Over", True, (255, 0, 0))
                 screen.blit(txt, [WIDTH//2-150, HEIGHT//2])
-                pg.display.update()
-                time.sleep(1)
-                return
-        
-        for bomb in bombs:
-            if bird.rct.colliderect(bomb.rct):
-                # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
-                bird.change_img(8, screen)
                 pg.display.update()
                 time.sleep(1)
                 return
